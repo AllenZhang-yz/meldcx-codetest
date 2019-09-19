c5dd46784edf0c3e9d9c027f5524769fc13b3e4c
Allen Zhang <allen.zhang018@gmail.com>
2019-09-19 16:02:18 +1000

    a minor bug fix

--- src/components/Devices/Devices.test.js
+++ src/components/Devices/Devices.test.js
@@ -8,24 +8,24 @@ import ErrorMsg from '../ErrorMsg';
 Enzyme.configure({ adapter: new Adapter() });
 
 describe('Devices', () => {
-    let component;
-    beforeEach(() => {
-        component = mount(<Devices />);
-    });
+  let component;
+  beforeEach(() => {
+    component = mount(<Devices />);
+  });
 
-    it('It should render 2 buttons without errors', () => {
-        const wrapper = component.find('button');
-        expect(wrapper.length).toBe(2);
-    });
+  it('It should render 2 buttons without errors', () => {
+    const wrapper = component.find('button');
+    expect(wrapper.length).toBe(2);
+  });
 
-    it('It should render CirclingBall without errors', () => {
-        const wrapper = component.find(CirclingBall);
-        expect(wrapper.length).toBe(1);
-    });
+  it('It should render CirclingBall without errors', () => {
+    const wrapper = component.find(CirclingBall);
+    expect(wrapper.length).toBe(1);
+  });
 
-    it('It should render error msg when can not fetch data', () => {
-        component.setState({ isFetchingDataErr: true });
-        const wrapper = component.find(ErrorMsg);
-        expect(wrapper.length).toBe(1);
-    });
+  it('It should render error msg when can not fetch data', () => {
+    component.setState({ isFetchingDataErr: true });
+    const wrapper = component.find(ErrorMsg);
+    expect(wrapper.length).toBe(1);
+  });
 });
